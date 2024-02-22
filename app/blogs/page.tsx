7c31a5cdbdd03762572d2e4c499d2da06c05d2ca
Andrew Grant <andrewgrant2009@hotmail.com>
2024-02-22 22:16:44 +0100

    on vercel and working?

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,35 +1,46 @@
 import { createClient } from "@/utils/supabase/server";
 
 export default async function Page() {
+  const supabase = createClient();
 
+
+
+
+
+ const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
 async function getMyBlogs() {
 
-  const supabase = createClient();
   const { data, error } = await supabase
-  .from("blogs")
-  .select(`id, title, content`);
+  .from("safeBlogs")
+  .select(`id, title, content, user_id`);
 
 return data
 }
 
 const myBlogs = await getMyBlogs();
 
-
+//  const filteredBlogs = myBlogs?.filter((bloggs) => bloggs.user_id === user?.id)
    // console.log(typeof(blogs))
-  return (
+  return user ? (
     <>
+<span className="text-3xl font-extrabold">Hello {user.email}</span>
 
 {myBlogs?.map(blog => (
-
 <div key={blog.id} className="m-2 p-2 border border-slate-900 rounded">
 <h1 className="m-2 text-2xl">{blog.title}</h1>
 <p>{blog.content}</p>
+<p>written by {blog.user_id}</p>
 </div>
 ))
 }
-
-      {/* <pre>{JSON.stringify(blogs, null, 2)}</pre> */}
     </>
-  );
+  ) : (
+
+<h1>Sign up, in or log in </h1>
+  )
 }
+
+{/* <pre>{JSON.stringify(blogs, null, 2)}</pre> */}
